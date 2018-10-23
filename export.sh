a5ef28e2de1bf99bc60faa1bb8722f77bf9c49c6
CaptainKoopa <Sharp.Brandon@gmail.com>
2018-10-23 16:46:26 -0400

    Added shell script

---
+++ export.sh
@@ -0,0 +1,20 @@
+now=`date '+%Y%m%d%H%M%S'`
+filename="/home/pi/movesA${now}.csv"
+echo $filename
+sqlite3 -csv /home/pi/floorlocation.db "SELECT * FROM moves;" > $filename
+if [ -z "$(ls -A /mnt/csvMove)" ]
+then
+        echo "Folders Empty, trying to mount, or check if DONTREMOVE.txt is missing from newmas share. Will try to copy again in one minute"
+        sudo mount -t cifs //NEWMAS/FloorLocations/IncomingFiles/ /mnt/csvMove/ -o user=FLocations,password=%YOULLNEEDTOCHECKCW%
+else
+        echo "Folder Not Empty, attempting to copy"
+        if [ -s $filename ]
+        then
+                echo 'File isnt empty, trying to copy and wipe database'
+                sudo cp $filename /mnt/csvMove/
+                sqlite3 /home/pi/floorlocation.db "DELETE FROM moves;"
+        else
+                echo 'Files empty, nothing is copying'
+                sudo rm $filename
+        fi
+fi
