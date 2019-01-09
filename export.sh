9d7b0c4c518b115f202b2a64a936daa069cc424b
Brandon <Sharp.Brandon@gmail.com>
2019-01-09 11:43:14 -0500

    Fixed export file to remove empties

--- export.sh
+++ export.sh
@@ -1,12 +1,13 @@
 now=`date '+%Y%m%d%H%M%S'`
-filename="/home/pi/movesA${now}.csv"
+filename="/home/pi/A${now}.csv"
 echo $filename
+echo `date`
 sqlite3 -csv /home/pi/floorlocation.db "SELECT * FROM moves;" > $filename
 sed -i 's/\"//g' $filename
 if [ -z "$(ls -A /mnt/csvMove)" ]
 then
         echo "Folders Empty, trying to mount, or check if DONTREMOVE.txt is missing from newmas share. Will try to copy again in one minute"
-        sudo mount -t cifs //NEWMAS/FloorLocations/IncomingFiles/ /mnt/csvMove/ -o user=FLocations,password=%YOULLNEEDTOCHECKCW%
+        sudo mount -t cifs //NEWMAS/FloorLocations/IncomingFiles/ /mnt/csvMove/ -o user=FLocations,password=APT.248.YEAH.BOI
 else
         echo "Folder Not Empty, attempting to copy"
         if [ -s $filename ]
@@ -14,8 +15,21 @@ else
                 echo 'File isnt empty, trying to copy and wipe database'
                 sudo cp $filename /mnt/csvMove/
                 sqlite3 /home/pi/floorlocation.db "DELETE FROM moves;"
-        else
+		sleep 5
+		sudo rm $filename
+	else
                 echo 'Files empty, nothing is copying'
+		sleep 5
                 sudo rm $filename
-        fi
+	fi
 fi
+#for some reason, some files would remain and be empty, this should remove those from now on
+for csv in /home/pi/*.csv; do
+	if [ -s $csv ]
+	then
+		rm -f $csv
+		echo 'Removed empty file $csv'
+	else
+		echo '$ not empty...'
+	fi
+done
---
+++ wlanMonitor.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if ifconfig wlan0 | grep -q "inet 10" ; then
+	echo "Seems ok"
+	echo `date`
+else
+	echo "Network is down again, attempting to rejoin"
+	echo `date`
+	sudo ifdown wlan0
+	sleep 10
+	sudo ifup --force wlan0
+	sleep 10
+fi
