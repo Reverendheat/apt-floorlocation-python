56c0afec41715a6c22b7e3b6783b3b6c34ac2d76
captainkoopa <parakoopa@Parakoopa-Desktop.localdomain>
2019-01-09 19:51:16 -0500

    Rearranged logging order

--- wlanMonitor.sh
+++ wlanMonitor.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 if ifconfig wlan0 | grep -q "inet 10\|inet 192\|inet 172" ; then
+	echo `date`
 	echo "Seems ok"
-	echo `date`
 else
-	echo "Network is down again, attempting to rejoin"
 	echo `date`
+	echo "Network is down again, attempting to rejoin"
 	sudo ifdown wlan0
 	sleep 10
 	sudo ifup --force wlan0
