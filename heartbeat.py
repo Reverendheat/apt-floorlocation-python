3485aa1e6aea1113caaa2ede8cfc59b8c8ef7d50
captainkoopa <parakoopa@Parakoopa-Desktop.localdomain>
2019-01-30 12:41:03 -0500

    Added heartbeatpy

---
+++ heartbeat.py
@@ -0,0 +1,16 @@
+#Check in with nodeJS Server
+import asyncio
+import socket
+
+async def heartbeat(message, loop):
+    reader, writer = await asyncio.open_connection('192.168.20.2', 10000,
+                                                   loop=loop)
+    writer.write(message.encode())
+    data = await reader.read(100)
+    writer.close()
+
+hostname = socket.gethostname()
+message = f'{hostname} is online!'
+loop = asyncio.get_event_loop()
+loop.run_until_complete(heartbeat(message, loop))
+loop.close()
