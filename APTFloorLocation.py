a9772b95b821d8a1801693264de6bb483be4f21e
captainkoopa <parakoopa@DESKTOP-GEEEPTK.localdomain>
2019-05-31 15:58:46 -0400

    Changed import type

--- APTFloorLocation.py
+++ APTFloorLocation.py
@@ -2,6 +2,8 @@ import csv
 import sqlite3
 import datetime 
 import os
+import wipfl
+import ebk
 from colorama import init
 from colorama import Fore, Back, Style
 init()
@@ -108,11 +110,13 @@ def mainFunction():
         print(Fore.YELLOW + Style.BRIGHT + "You just cleared the current session")
         mainFunction()
     elif (weighBIN in mainInput):
-        os.system('python3 /home/pi/aptfloorlocationpython/ebk.py')
+        #os.system('python3 /home/pi/aptfloorlocationpython/ebk.py')
         #os.system('clear')
+        ebk.main()
         mainFunction()
     elif (wipFL in mainInput):
-        os.system('python3 /home/pi/aptfloorlocationpython/wipfl.py')
+        #os.system('python3 /home/pi/aptfloorlocationpython/wipfl.py')
+        wipfl.main()
         #os.system('clear')
         mainFunction()
     elif not any(word in mainInput for word in locationWords):
