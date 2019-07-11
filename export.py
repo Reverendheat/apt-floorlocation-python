941833e19ec821a85bbc0af6b5ad9c017a6f6d0e
captainkoopa <parakoopa@DESKTOP-GEEEPTK.localdomain>
2019-07-11 15:55:24 -0400

    Fixed row deletion

--- export.py
+++ export.py
@@ -11,11 +11,12 @@ conn = pyodbc.connect('DSN=NAME1;UID=sa;PWD=%s;TDS_Version=7.4' % os.getenv("NEW
 conn.autocommit = False
 cursor = conn.cursor()
 
-sqlitecursor.execute('SELECT * FROM wipfl')
+sqlitecursor.execute('SELECT rowid, * FROM wipfl')
 for row in sqlitecursor:
-    cursor.execute("EXEC dbo.ABW_WIP_BinInsert @EmpID = {}, @FilledBinWeight = [{}], @PartNum = [{}], @ScanCode0 = [{}], @ScanCode1 = [{}], @ScanCode2 = [{}], @ScaleIp = [{}], @SourceLocation = [{}], @DestinationLocation = [{}], @BinType = [{}] ".format(row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[9]))
-    sqlitecursor.execute("DELETE FROM tasks WHERE id=?", row.id)
-    sqlitecursor.commit()
+    cursor.execute("EXEC dbo.ABW_WIP_BinInsert @EmpID = {}, @FilledBinWeight = [{}], @PartNum = [{}], @ScanCode0 = [{}], @ScanCode1 = [{}], @ScanCode2 = [{}], @ScaleIp = [{}], @SourceLocation = [{}], @DestinationLocation = [{}], @BinType = [{}] ".format(row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[0],row[10]))
+    sqlitecursor.execute('DELETE FROM wipfl WHERE rowid={}'.format(row[0]))
 
+db.commit()
 conn.commit()
-conn.close()   
+conn.close()
+db.close()
