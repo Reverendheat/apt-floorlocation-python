9a98f637efc7e0508df1f7f3ef25ded34565aabd
Vorunyets <woroniec@icloud.com>
2019-07-15 05:54:19 -0400

    column fix

--- SQLServerClasses.py
+++ SQLServerClasses.py
@@ -87,7 +87,7 @@ class SQLServerFunctions:
         theTime = '{0:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())
         theTime.replace("'",'')
         #Connect to local SQLITE DB and store values
-        cursor.execute('INSERT INTO wipfl(empID, FilledBinWeight, WipNum, ScanCode0, ScanCode1, ScanCode2, ScaleIp, sourceLoc, destLoc, typeOfBin) VALUES(?,?,?,?,?,?,?,?,?,?,?)',(EmpId, FilledBinWeight, WipNum, ScanCode0, ScanCode1, ScanCode2,ScaleIp,sourceLoc,destLoc,typeOfBin,theTime))
+        cursor.execute('INSERT INTO wipfl(empID, FilledBinWeight, WipNum, ScanCode0, ScanCode1, ScanCode2, ScaleIp, sourceLoc, destLoc, typeOfBin, timeEntered) VALUES(?,?,?,?,?,?,?,?,?,?,?)',(EmpId, FilledBinWeight, WipNum, ScanCode0, ScanCode1, ScanCode2,ScaleIp,sourceLoc,destLoc,typeOfBin,theTime))
         db.commit()
         #conn = pyodbc.connect('DSN=NAME1;UID=sa;PWD=lookincw;TDS_Version=7.4')
         
