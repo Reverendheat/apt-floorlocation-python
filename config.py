7b9a865d1acb5e998b9e6512725785649202b19c
Reverendheat <sharp.brandon@gmail.com>
2019-11-12 16:51:05 -0500

    Updated for heartbeat

--- config.py
+++ config.py
@@ -1,5 +1,17 @@
+import sqlite3
+
+db = sqlite3.connect('floorlocation.db')
+cursor = db.cursor()
+
+Cart = cursor.execute('SELECT cart FROM moves WHERE rowid = (SELECT MAX(rowid) FROM moves);')
+Cart = cursor.fetchall()
+Employee = cursor.execute('SELECT empID FROM empinfo;')
+Employee = cursor.fetchall()
+
 settings = {
-    "Version" : "1.7.5",
+    "Version" : "1.7.9",
+    "Cart" : Cart[0][0],
+    "Employee" : Employee[0][0],
     "Server" : "wl-scanning",
     "Port" : "10000",
     "DevServer" : "localhost"
--- heartbeat.py
+++ heartbeat.py
@@ -1,17 +1,25 @@
 #Check in with nodeJS Server
 import asyncio
 import socket
+import json
 from config import settings
 
 async def heartbeat(message, loop):
-    reader, writer = await asyncio.open_connection(settings["Server"], settings["Port"],
+    reader, writer = await asyncio.open_connection(settings["DevServer"], settings["Port"],
                                                    loop=loop)
     writer.write(message.encode())
     data = await reader.read(100)
     writer.close()
 
 hostname = socket.gethostname()
-message = hostname + ' is online! ' + settings["Version"]
+message = {
+    "hostname" : hostname,
+    "version" : settings["Version"],
+    "cart" : settings["Cart"],
+    "employee" : settings["Employee"]
+}
+message = json.dumps(message)
+#message = hostname + ' is online! ' + settings["Version"] + " Cart: " + settings["Cart"] + " Employee: " + settings["Employee"]
 loop = asyncio.get_event_loop()
 try:
     loop.run_until_complete(heartbeat(message, loop))
